aec0b2e21586ec0fe32ddc8e1ec1643661569d35
papalanya <illiampavlenko.gmail.com>
2022-11-20 12:15:36 +0200

    add API and Endless scroll

--- src/index.js
+++ src/index.js
@@ -1,7 +1,34 @@
 import './sass/main.scss';
 import Notiflix from 'notiflix';
-const axios = require('axios').default;
 
 import SimpleLightbox from "Simplelightbox";
 // Дополнительный импорт стилей
 import "simplelightbox/dist/simple-lightbox.min.css";
+
+import PixabayAPI from './js/PixabayAPI.js';
+
+const refs = {
+    form: document.querySelector('#search-form'),
+    gallery: document.querySelector('.gallery'),
+    searchEnd: document.querySelector('.search-end'),
+}
+
+const { form, gallery, searchEnd } = refs;
+
+const pixabayAPI = new PixabayAPI();
+
+form.addEventListener('submit', onSearch);
+
+let shownImages = 0;
+let lightbox = {};
+
+const endlessScroll = new IntersectionObserver(
+    ([entry], observer) => {
+        if (entry.isIntersecting) {
+            observer.unobserve(entry.target);
+
+            loadMore();
+        }
+    },
+    { root: null, rootMargin: '50px', threshold: 0.5 },
+);
