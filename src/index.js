cff62e7956b8027e6d5773d145f312269da55b2c
papalanya <illiampavlenko.gmail.com>
2022-11-20 13:07:58 +0200

    complete tasks withot styles

--- src/index.js
+++ src/index.js
@@ -32,3 +32,103 @@ const endlessScroll = new IntersectionObserver(
     },
     { root: null, rootMargin: '50px', threshold: 0.5 },
 );
+
+function addObserve() {
+    const lastImage = document.querySelector('.photo-card:last-child');
+
+    if (lastImage) {
+        endlessScroll.observe(lastImage);
+    }
+}
+
+function exodus(totalHits) {
+    if (shownImages < totalHits) {
+        addObserve();
+    } else {
+        showSearchEnd();
+    }
+}
+
+function showSearchEnd() {
+    searchEnd.classList.remove('is-hidden')
+}
+
+function hideSearchEnd() {
+    searchEnd.classList.add('is-hidden');
+}
+
+function resetGallery() {
+    gallery.innerHTML = '';
+}
+
+function addLightbox() {
+    lightbox = new SimpleLightbox('.gallery a', {
+        showCounter: false,
+        captionsData: 'alt',
+        captionsDelay: 250,
+    });
+}
+
+async function onSearch(e) {
+    e.preventDefault();
+    resetGallery();
+    pixabayAPI.resetPage();
+    hideSearchEnd();
+
+    pixabayAPI.query = form.elements.searchQuery.value.trim();
+
+    try {
+        const data = await pixabayAPI.fetchQuery();
+
+        if (data.totalHits === 0) {
+            return Notiflix.Notify.failure(
+                'Sorry, there are no images matching your search query. Please try again.'
+            );
+        }
+
+        shownImages = data.hits.length;
+        Notiflix.Notify.success(
+            `Hooray! We found ${data.totalHits} images.`
+        );
+        appendPhotosMarkup(data.hits);
+        addLightbox();
+        exodus(data.totalHits);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function appendPhotosMarkup(cards) {
+    gallery.insertAdjacentHTML('beforeend', PhotosMarkup(cards));
+}
+
+async function loadMore() {
+    try {
+        const data = await pixabayAPI.fetchQuery();
+        appendPhotosMarkup(data.hits);
+        lightbox.refresh();
+
+        shownImages += data.hits.length;
+
+        exodus(data.totalHits);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function PhotosMarkup(cardsArray) {
+    return cardsArray
+        .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+            return `
+                <a href="${largeImageURL}" class="photo-card">
+                    <img src="${webformatURL}" alt="${tags}" class="photo-card__img" width="300" loading="lazy" />
+                    <div class="info">
+                        <p class="info-item"><b>Likes</b><br />${likes}</p>
+                        <p class="info-item"><b>Views</b><br />${views}</p>
+                        <p class="info-item"><b>Comments</b><br />${comments}</p>
+                        <p class="info-item"><b>Downloads</b><br />${downloads}</p>
+                    </div>
+                </a>
+            `;
+        }).join('');
+}
